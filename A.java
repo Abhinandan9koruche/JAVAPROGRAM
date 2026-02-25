a4728442c2c98f2cfb1bfe8e03a81201c967e37a
Abhinandan <abhikoruche9@gmail.com>
2026-02-26 03:04:03 +0530

    Missing Element program

---
+++ A.java
@@ -0,0 +1,13 @@
+public class A{
+    public static void main(String[] args) {
+        int a[]={1,2,4,5,6};
+        int exce=a.length*(a.length+1)/2;
+        
+        int result=0;
+        for(int i=0;i<a.length;i++){
+            result=result+a[i];
+        }
+
+        System.out.println(result-exce +" is the  missing Element in the list");
+    }
+}
